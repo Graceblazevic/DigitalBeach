96a6e8c28cde97518123e2bbdc5776a989a7378c
grace blazevic <graceb99@hotmail.com>
2020-10-29 11:42:18 +1300

    new for background image

--- js/book.js
+++ js/book.js
@@ -2,3 +2,58 @@ $(document).ready(function () {
   // All elements with the "draggy" class become draggable (and on mobile)
   $('.draggy').draggable();
 });
+
+
+// new for background image drag //
+
+$(document).ready(function(){
+    var $bg = $('.bg-img'),
+        origin = {x: 0, y: 0},
+        start = {x: 0, y: 0},
+        movecontinue = false;
+
+    function move (e){
+        var moveby = {
+            x: origin.x - e.clientX,
+            y: origin.y - e.clientY
+        };
+
+        if (movecontinue === true) {
+            start.x = start.x - moveby.x;
+            start.y = start.y - moveby.y;
+
+            $(this).css('background-position', start.x + 'px ' + start.y + 'px');
+        }
+
+        origin.x = e.clientX;
+        origin.y = e.clientY;
+
+        e.stopPropagation();
+        return false;
+    }
+
+    function handle (e){
+        movecontinue = false;
+        $bg.unbind('mousemove', move);
+
+        if (e.type == 'mousedown') {
+            origin.x = e.clientX;
+            origin.y = e.clientY;
+            movecontinue = true;
+            $bg.bind('mousemove', move);
+        } else {
+            $(document.body).focus();
+        }
+
+        e.stopPropagation();
+        return false;
+    }
+
+    function reset (){
+        start = {x: 0, y: 0};
+        $(this).css('backgroundPosition', '0 0');
+    }
+
+    $bg.bind('mousedown mouseup mouseleave', handle);
+    $bg.bind('dblclick', reset);
+});
